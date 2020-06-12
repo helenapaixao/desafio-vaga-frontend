683abca2a3d3fd407aabbecbe861b2898d4f70a3
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-12 05:16:22 -0400

    page signin ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function App() {
-  return <h1>Hello</h1>;
-}
+import SignIn from './pages/SignIn'
+import GlobalStyle from './styles/global'
 
-export default App;
+
+function App() {
+    return (
+        <>
+        <SignIn/>
+        <GlobalStyle/>
+        </>
+    )
+  }
+  
+  export default App;
