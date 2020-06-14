5e05076f4a2a735be21e351ab526034a50e4e637
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-14 01:00:39 -0400

    criando header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower } from 'react-icons/fi';
 
+const Dashboard: React.FC = () => (
+    <Container>
+        <Header>
+            <HeaderContent>
+                <Profile>
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/11083288?s=460&u=195f820bdb85e57d7e08038a3f8eec821421d83d&v=4"
+                        alt="Helena Paixão"
+                    />
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>
-
+                    <div>
+                        <span>Bem-vindo,</span>
+                        <strong>Helena Paixão</strong>
+                    </div>
+                </Profile>
+                <button type="button">
+                    <FiPower />
+                </button>
+            </HeaderContent>
+        </Header>
+    </Container>
+);
 
 export default Dashboard;
